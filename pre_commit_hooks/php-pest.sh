a659ac9ffa300b688d6974a2592c633a4c252e39
Diana van der Schouw <diana.vds@trafficsupply.nl>
2024-02-09 14:36:43 +0100

    Refactor pre-commit hooks for PestPHP and PHPStan
    - Added `--parallel` argument to the `php-pest` hook
    - Added `--parallel --coverage --min=100` arguments to the `php-pest-coverage` hook
    - Removed unused script file `pre_commit_hooks/php-pest-coverage.sh`
    - Modified the logic in `pre_commit_hooks/php-pest.sh` to accept command line arguments and files to check

--- pre_commit_hooks/php-pest.sh
+++ pre_commit_hooks/php-pest.sh
@@ -15,14 +15,15 @@
 msg_color_magenta='\e[1;35m'
 msg_color_yellow='\e[0;33m'
 msg_color_none='\e[0m' # No Color
-
 echo -en "${msg_color_yellow}Begin PestPHP Task Runner ...${msg_color_none} \n"
 
 phpunit_command="vendor/bin/sail pest"
 
+files_to_check="${@:2}"
+args=$1
 
-echo "Running command $phpunit_command"
-$phpunit_command
+echo "Running command $phpunit_command $args"
+$phpunit_command $args
 if [ $? -ne 0 ]; then
     exit 1
 fi
