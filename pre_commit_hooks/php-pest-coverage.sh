b7ad89cdf751c481ae9dc03cd020fe4713e0eb48
Diana van der Schouw <diana.vds@trafficsupply.nl>
2024-02-09 12:00:25 +0100

    feat: Add PestPHP, PestPHP Coverage, Laravel Pint and PHPStan pre-commit hooks
    - Added new pre-commit hooks for running the full php unit test with PestPHP, running the full php unit test with PestPHP with coverage, linting your Laravel project with Laravel Pint and checking for bugs in your code using PHPStan. All of these need Sail to run before passing, except for PHPStan.
    - Updated .pre-commit-hooks.yaml to include the new hooks.
    - Added scripts for each hook in the pre_commit_hooks directory.

---
+++ pre_commit_hooks/php-pest-coverage.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Bash PHP Unit Task Runner
+#
+# Exit 0 if no errors found
+# Exit 1 if errors were found
+#
+# Requires
+# - php
+#
+# Arguments
+# - None
+
+# Echo Colors
+msg_color_magenta='\e[1;35m'
+msg_color_yellow='\e[0;33m'
+msg_color_none='\e[0m' # No Color
+
+echo -en "${msg_color_yellow}Begin PestPHP Task Runner ...${msg_color_none} \n"
+
+phpunit_command="vendor/bin/sail pest --coverage"
+
+
+echo "Running command $phpunit_command"
+$phpunit_command
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+exit 0
